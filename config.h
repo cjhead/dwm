8cfc994d7ca5cad218cc27d7e4320e6c46bab807
cjhead <chead@linuxmail.org>
2022-02-23 08:15:44 -0500

    Adds the swallow patch

--- config.h
+++ config.h
@@ -3,6 +3,7 @@
 /* appearance */
 static unsigned int borderpx        = 1;        /* border pixel of windows */
 static unsigned int snap            = 32;       /* snap pixel */
+static const int swallowfloating    = 0;        /* 1 means swallow floating windows by default */
 static const unsigned int gappih    = 0;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 0;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 0;       /* horiz outer gap between windows and screen edge */
@@ -67,9 +68,14 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	/* class     instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
+	{ "Gimp",      NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Firefox",   NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
+	{ "St",        NULL,     NULL,           0,         0,          1,           0,        -1 },
+    { "Brave",     NULL,     NULL,           0,         0,          0,           1,        -1 },
+    { "Tk",        NULL,     NULL,           0,         1,          0,           1,        -1 },
+    { "python3.9", NULL,     NULL,           0,         1,          0,           1,        -1 },
+	{ NULL,        NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
 };
 
 /* layout(s) */
