d5bc84a3dc70dc527c1e484433ad194b14da5d91
cjhead <chead@linuxmail.org>
2022-09-21 09:04:49 -0400

    feat: adds attachdirection patch

--- config.h
+++ config.h
@@ -85,6 +85,7 @@ static int nmaster              = 1;    /* number of clients in master area */
 static int resizehints          = 0;    /* 1 means respect size hints in tiled resizals */
 static const int decorhints     = 1;    /* 1 means respect decoration hints */
 static const int lockfullscreen = 1;    /* 1 will force focus on the fullscreen window */
+static const int attachdirection = 2;    /* 0 default, 1 above, 2 aside, 3 below, 4 bottom, 5 top */
 
 static const Layout layouts[] = {
 	/* symbol     arrange function */
